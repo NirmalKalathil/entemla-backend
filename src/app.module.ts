fd037ae907ed2a8a3153d530d2fa534a931c53b6
navyavm123-sudo <navyavm123@gmail.com>
2026-05-28 20:19:34 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleModule } from '@nestjs/schedule';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { AuthModule } from './auth/auth.module';
 import { ComplaintsModule } from './complaints/complaint.module';
 import { UsersModule } from './users/users.module';
-import { RagModule } from './rag/rag.module';
-
-import { AdminModule } from './admin/admin.module';
-
+import { ChatModule } from './chat/chat.module'; 
 @Module({
   imports: [
-    ScheduleModule.forRoot(),
-
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         uri: config.get<string>('MONGODB_URI'),
       }),
     }),
-
     AuthModule,
     ComplaintsModule,
-    RagModule,
     UsersModule,
-    AdminModule,
+    ChatModule, 
   ],
-
   controllers: [AppController],
-
   providers: [AppService],
 })
 export class AppModule {}
